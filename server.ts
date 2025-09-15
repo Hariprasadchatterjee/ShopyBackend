e4ab0d4c4883ac71a8e57a682e76ba5c07bfd672
Hariprasad <harichatterjee957@gmail.com>
2025-09-15 10:06:13 +0530

    add app file

--- server.ts
+++ server.ts
@@ -1,6 +1,11 @@
-console.log("welcome");
+import app from "./src/app.js"
 
-function name() {
-  console.log("Asdasd");
+const startServer =()=>{
+  const PORT = 3000;
+  app.listen(PORT, ()=>{
+    console.log(`listinig to port ${PORT}`);
+    
+  })
 }
-name();
+
+startServer()
