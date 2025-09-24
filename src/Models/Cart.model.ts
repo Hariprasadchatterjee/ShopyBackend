40d5bf47a985776a239121d03cf4acae3e352260
Hariprasad <harichatterjee957@gmail.com>
2025-09-24 16:54:00 +0530

    add cart model

---
+++ src/Models/Cart.model.ts
@@ -0,0 +1,47 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+export interface ICartItem extends Document {
+  product: mongoose.Schema.Types.ObjectId;
+  quantity: number;
+}
+export interface ICart extends Document {
+  user: mongoose.Schema.Types.ObjectId;
+  items: ICartItem[];
+  subtotal: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const cartItemSchema = new Schema<ICartItem>({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+    default: 1,
+  },
+});
+
+const cartSchema = new Schema<ICart>(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true, // Each user can only have one cart
+    },
+    items: [cartItemSchema],
+    subtotal: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const Cart: Model<ICart> = mongoose.model<ICart>("Cart", cartSchema);
+export default Cart;
